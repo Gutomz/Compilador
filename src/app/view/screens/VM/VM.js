7ddddf8810b540ee000ec54f278e44fe28f22570
Gutomz <gutomz@outlook.com>
2020-08-21 11:44:05 -0300

    feat(file): load file test

--- src/app/view/screens/VM/VM.js
+++ src/app/view/screens/VM/VM.js
@@ -1,15 +1,49 @@
 import React from 'react';
 
-import { Input, Button } from 'antd';
+import { Input, Button, Statistic, notification } from 'antd';
+
+const app = window.require('electron').remote;
+const fs = app.require('fs');
+const { dialog } = app;
 
 class VM extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      data: [],
+    };
   }
 
+  onLoadFilePress = async () => {
+    const file = await dialog.showOpenDialog({
+      title: 'Selecionar Arquivo',
+      buttonLabel: 'Selecionar',
+      filters: [{ name: 'Arquivo Texto .txt', extensions: ['txt'] }],
+    });
+
+    if (file.canceled || file.filePaths.length === 0) {
+      console.log('Error ao carregar o arquivo');
+      notification.error({
+        message: 'Error ao carregar o arquivo',
+        placement: 'topRight',
+      });
+      return;
+    }
+
+    fs.readFile(file.filePaths[0], 'utf8', (err, data) => {
+      if (err) {
+        console.log('Error ao carregar o arquivo');
+        return;
+      }
+      this.setState({ data: data.split('\n') });
+      console.log(data.split('\n'));
+    });
+  };
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className="vm">
         <div className="vm__inner-view vm__inner-view--first">
@@ -29,10 +63,29 @@ class VM extends React.Component {
         <div className="vm__inner-view vm__inner-view--second">
           <div className="vm__code-view">
             <div className="vm__code-view__inner">
-              <p className="vm__code-view__inner__text">Código</p>
+              {data.map((line) => (
+                <p className="vm__code-view__inner__text">{line}</p>
+              ))}
             </div>
           </div>
-          <div className="vm__input-view" />
+          <div className="vm__input-view">
+            <Button onClick={() => this.onLoadFilePress()}>
+              Carregar Arquivo
+            </Button>
+            <div className="vm__input-view__button">
+              <Statistic
+                title="Registrador de Programa"
+                value={0}
+                precision={0}
+              />
+              <Statistic
+                className="vm__input-view__button--margin-left"
+                title="Topo da Pilha"
+                value={0}
+                precision={0}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
