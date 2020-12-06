b11b9eb988d47ce18071bc237267d144ece54d6f
José <josedpires@hotmail.com>
2020-12-06 00:11:58 -0300

    feat: instructions

--- src/app/redux/actions/vm.js
+++ src/app/redux/actions/vm.js
@@ -82,3 +82,225 @@ export const executeLDV = (n) => (dispatch, getState) => {
 
 // Pegar valor de uma posição do vetor de dados
 // const a = VMSelectors.getDataStackByPosition(getState(), dataPointer - 1)
+
+export const executeADD = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState());
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  );
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  );
+  const newData = actualData + lastData;
+  dispatch(insertDataStack(newData, -1));
+  dispatch(decrementDataPointer());
+};
+
+export const executeSUB = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState());
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  );
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  );
+  const newData = actualData - lastData;
+  dispatch(insertDataStack(newData, -1));
+  dispatch(decrementDataPointer());
+};
+
+export const executeMULT = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState());
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  );
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  );
+  const newData = actualData * lastData;
+  dispatch(insertDataStack(newData, -1));
+  dispatch(decrementDataPointer());
+};
+
+export const executeDIVI = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState());
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  );
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  );
+  const newData = actualData / lastData;
+  dispatch(insertDataStack(newData, -1));
+  dispatch(decrementDataPointer());
+};
+
+export const executeINV = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  dispatch(insertDataStack(-actualData));
+};
+
+export const executeAND = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  if (lastData === 1 && actualData === 1) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeOR = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  if (lastData === 1 || actualData === 1) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeNEG = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  dispatch(insertDataStack(1 - actualData));
+};
+
+export const executeCME = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData < actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeCMA = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData > actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeCEQ = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData === actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeCDIF = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData !== actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeCMEQ = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData <= actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
+
+export const executeCMAQ = () => (dispatch, getState) => {
+  const dataPointer = VMSelectors.getInstructionPointer(getState()); // s
+  const actualData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer
+  ); // M[s]
+  const lastData = VMSelectors.getDataStackByPosition(
+    getState(),
+    dataPointer - 1
+  ); // M[s - 1]
+  if (lastData >= actualData) {
+    dispatch(insertDataStack(1, -1));
+  } else {
+    dispatch(insertDataStack(0, -1));
+  }
+  dispatch(decrementDataPointer());
+};
